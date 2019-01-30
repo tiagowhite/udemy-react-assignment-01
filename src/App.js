9a29f82cdae39ff779cdd9b16726b8e6671c04cd
Tiago Fernandes <tiagowhite@gmail.com>
2019-01-30 20:12:13 -0200

    playing with lists

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, {Component} from 'react';
-import './App.scss';
+import uuidv4 from 'uuid/v4';
 import Person from './Person/Person';
-import {UserOutput, UserInput} from "./User/User";
-import * as names from './Person/Names';
+import './App.scss';
 
 class App extends Component {
 
-    randomNames = names['default'];
-
     state = {
         persons: [
-            {
-                name: this.randomNames[Math.floor(Math.random() * this.randomNames.length)],
-                age: Math.floor(Math.random() * Math.floor(57))
-            }
+
+            {id: uuidv4(), name: 'Isaac', age: 23},
+            {id: uuidv4(), name: 'Isobel', age: 47},
+            {id: uuidv4(), name: 'Ferdinand', age: 18}
         ],
-        username: 'morty'
+        username: 'morty',
+        showPersons: false
     };
     //teken from https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
     fibonacci = (num, memo) => {
@@ -27,67 +25,45 @@ class App extends Component {
         return memo[num] = this.fibonacci(num - 1, memo) + this.fibonacci(num - 2, memo);
     };
 
-    switchNameHandler = () => {
-        this.setState({
-            persons: [
-                {
-                    name: this.randomNames[Math.floor(Math.random() * this.randomNames.length)],
-                    age: Math.floor(Math.random() * Math.floor(57))
-                }
-            ]
-        })
+
+    deletePersonHandler = (personIndex) => {
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons})
     };
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                {
-                    name: event.target.value,
-                    age: Math.floor(Math.random() * Math.floor(57))
-                }
-            ]
-        })
+    togglePersonsHandler = () => {
+        const doesshow = this.state.showPersons;
+        this.setState({showPersons: !doesshow});
+        console.log(uuidv4());
     };
 
-
-    userInputHandler = (event) => {
-        this.setState({
-                username: event.target.value
-            }
-        )
-    };
 
     render() {
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return <Person
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}
+                            key={person.id}
+                        />
+                    })}
+                </div>
+            )
+        }
+
         return (
             <div className="App">
                 <h1>Hello i'm a robot</h1>
                 <p>this is looking good</p>
-                <button onClick={this.switchNameHandler}>Switch name</button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}
-                    click={this.switchNameHandler}
-                    changed={this.nameChangedHandler}
-                />
-                <UserInput
-                    changed={this.userInputHandler}
-                />
-                <UserOutput
-                    text={"this is a dummy text passed as argument to 'userOutput' component"}
-                />
-                <UserOutput
-                    text={"this is a random fibonaci sequence: " + this.fibonacci(Math.random() * 50).toString()}
-                />
-                <UserOutput
-                    text={"Username:"}
-                    username={"Rick Sanchez"}
-                />
-                <UserOutput
-                    text={"Username via state binding: "}
-                    username={this.state.username}
-
-                />
-
+                <button onClick={this.togglePersonsHandler}>Switch name</button>
+                {persons}
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,6 @@ const person = (props) => {
         <div className="Person">
             <p className="name" onClick={props.click}> Hello! my name is {props.name}, and i'm {props.age} years old! </p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
